8d6d4c2de8cb83b26c9ed2fec826867710c4bdcb
MachineTheSlayer <dangodggw@gmail.com>
2025-10-14 23:51:33 +0400

    add setCurrentStep

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import type { ThunkAction, Action } from "@reduxjs/toolkit"
 import { configureStore } from "@reduxjs/toolkit"
 
+import playgroundReducer from "../components/Playground/store/slices"
+
 export const store = configureStore({
   reducer: {
-    //
+    playground: playgroundReducer,
   },
 })
 
